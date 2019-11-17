ec244db0abdc9826fc7298ae30b1dc507229cef2
tbrunton <bruntontom131@gmail.com>
2019-11-16 21:26:48 -0500

    #3 Added main menu screen, game screen and settings screen

--- app/src/main/java/com/example/cps731_project/MainActivity.java
+++ app/src/main/java/com/example/cps731_project/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.cps731_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.cps731.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startGameButton(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    public void settingsButton(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
