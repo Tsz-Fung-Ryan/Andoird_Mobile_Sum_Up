f6c2abad7acdabc3e4711467079626585ab84d68
Tsz-Fung-Ryan Hoang <39393143+Tsz-Fung-Ryan@users.noreply.github.com>
2019-11-19 11:53:15 -0500

    finished with constructors, getters,setters Began working on answer generator

--- app/src/main/java/com/example/cps731_project/Equations.java
+++ app/src/main/java/com/example/cps731_project/Equations.java
@@ -1,4 +1,6 @@
 package com.example.cps731_project;
+import java.util.Random;
+
 
 public class Equations {
 
@@ -8,11 +10,53 @@ public class Equations {
 
     public Equations(int [] numbers){
         this.numbers = numbers;
-        modifiers = new char [] {'+','-'};
+        setModifiers(new char [] {'+','-'});
     }
 
     public Equations (int [] numbers, char [] modifiers){
+        setNumbers(numbers);
+        setModifiers(modifiers);
+    }
+
+    public void setNumbers(int[] numbers) {
         this.numbers = numbers;
+    }
+
+    public int [] getNumbers(){
+        return numbers;
+    }
+
+    public void setModifiers(char[] modifiers) {
         this.modifiers = modifiers;
+    }
+
+    public int generateAnswer(){
+        Random rand = new Random();
+        int answer;
+        numbers = getNumbers();
+        int randInt = rand.nextInt(numbers.length);
+
+        answer = numbers[randInt];
+        int [] newNumbers = new int[numbers.length-1];
+
+        for(int i = 0; i<newNumbers.length; i++){
+            if(i>=randInt)
+                newNumbers[i]=numbers[i+1];
+            else
+                newNumbers [i]= numbers[i];
+        }
+
+        //gonna have to do something with the modifier
+
+        answer = iterateAnswer(answer, newNumbers);
+        return answer;
+    }
+    public int iterateAnswer(int answer, int[]numbers){
+        if(numbers.length==0)
+            return answer;
+        else{
+
+        }
+        return answer;
     }
 }
