99d15b57035345fb99418b191b5dc07ac3cfb856
Tsz-Fung-Ryan Hoang <39393143+Tsz-Fung-Ryan@users.noreply.github.com>
2019-11-19 11:12:33 -0500

    constructors

--- app/src/main/java/com/example/cps731_project/Equations.java
+++ app/src/main/java/com/example/cps731_project/Equations.java
@@ -2,6 +2,17 @@ package com.example.cps731_project;
 
 public class Equations {
 
+    final int AMOUNTOFNUMBERS = 5;
+    int [] numbers = new int [AMOUNTOFNUMBERS];
+    char [] modifiers;
 
-    public Equations()
+    public Equations(int [] numbers){
+        this.numbers = numbers;
+        modifiers = new char [] {'+','-'};
+    }
+
+    public Equations (int [] numbers, char [] modifiers){
+        this.numbers = numbers;
+        this.modifiers = modifiers;
+    }
 }
