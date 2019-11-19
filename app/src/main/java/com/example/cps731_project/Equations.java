055f244ed935432fade8db7024a2e9b59642a43d
Tsz-Fung-Ryan Hoang <39393143+Tsz-Fung-Ryan@users.noreply.github.com>
2019-11-19 13:46:45 -0500

    Finished constructing answer generator

--- app/src/main/java/com/example/cps731_project/Equations.java
+++ app/src/main/java/com/example/cps731_project/Equations.java
@@ -4,10 +4,11 @@ import java.util.Random;
 
 public class Equations {
 
-    final int AMOUNTOFNUMBERS = 5;
-    int [] numbers = new int [AMOUNTOFNUMBERS];
-    char [] modifiers;
-    int answer;
+    private final int AMOUNTOFNUMBERS = 5;
+    private Random rand = new Random();
+    private int [] numbers = new int [AMOUNTOFNUMBERS];
+    private char [] modifiers;
+    private int answer;
 
     public Equations(int [] numbers){
         this.numbers = numbers;
@@ -30,9 +31,10 @@ public class Equations {
     public void setModifiers(char[] modifiers) {
         this.modifiers = modifiers;
     }
-
+    public char [] getModifiers(){
+        return modifiers;
+    }
     public int generateAnswer(){
-        Random rand = new Random();
         numbers = getNumbers();
         int randInt = rand.nextInt(numbers.length);
 
@@ -46,17 +48,48 @@ public class Equations {
                 newNumbers [i]= numbers[i];
         }
 
-        //gonna have to do something with the modifier
-
         answer = iterateAnswer(answer, newNumbers);
         return answer;
     }
+
     public int iterateAnswer(int answer, int[]numbers){
         if(numbers.length==0)
             return answer;
         else{
+            modifiers=getModifiers();
+            int randInt = rand.nextInt(numbers.length);
+            int operand = numbers[randInt];
+            randInt = rand.nextInt(modifiers.length);
+            char operator = modifiers[randInt];
 
+            answer = calculate(answer, operator, operand);
+
+            int [] newNumbers = new int[numbers.length-1];
+
+            for(int i = 0; i<newNumbers.length; i++){
+                if(i>=randInt)
+                    newNumbers[i]=numbers[i+1];
+                else
+                    newNumbers [i]= numbers[i];
+            }
+            answer = iterateAnswer(answer, newNumbers);
+            return answer;
         }
-        return answer;
+    }
+    private int calculate (int operand1, char modifier, int operand2){
+        int calculation=operand1;
+
+        switch(modifier) {
+            case '+':
+                calculation+=operand2;
+                break;
+            case '-':
+                calculation-=operand2;
+                break;
+            case 'x':
+                calculation= calculation*operand2;
+                break;
+        }
+        return calculation;
     }
 }
