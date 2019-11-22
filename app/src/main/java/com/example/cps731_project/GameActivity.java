40d785442e63478f03fdc06b8fde7846b82ae7db
tbrunton <thomas.brunton@ryerson.ca>
2019-11-22 12:42:27 -0500

    Added switching of element text for moving numbers into equation and more
    Added switching of element text for the generated numbers and the blank spaces for them. Also added cycling of operators with just + and - so far

--- app/src/main/java/com/example/cps731_project/GameActivity.java
+++ app/src/main/java/com/example/cps731_project/GameActivity.java
@@ -3,12 +3,49 @@ package com.example.cps731_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+
+    private TextView selectedView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+    }
+
+    public void elementClick(View view) {
+        TextView textView = (TextView) view;
+
+        if(selectedView == textView) {
+            textView.setTextSize(14);
+            selectedView = null;
+        } else if(selectedView != null) {
+            switchValues(selectedView, textView);
+            selectedView.setTextSize(14);
+            textView.setTextSize(14);
+            selectedView = null;
+        } else {
+            textView.setTextSize(28);
+            selectedView = textView;
+        }
+    }
+
+    public void switchValues(TextView view1, TextView view2) {
+        String temp = view1.getText().toString();
+        view1.setText(view2.getText());
+        view2.setText(temp);
+    }
+
+    public void cycleOperators(View view) {
+        TextView textView = (TextView) view;
+
+        if(textView.getText().toString().equals("+")) {
+            textView.setText("-");
+        } else {
+            textView.setText("+");
+        }
     }
 }
